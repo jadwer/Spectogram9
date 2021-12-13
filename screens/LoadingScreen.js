ade3b9ec3ce120f56c1a7326c9360c35d18d7081
Byjus5 <89545408+Byjus5@users.noreply.github.com>
2021-12-13 13:31:32 -0600

    Update LoadingScreen.js

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -14,9 +14,9 @@ export default class LoadingScreen extends Component {
     checkIfLoggedIn = () => {
         firebase.auth().onAuthStateChanged((user) => {
             if (user) {
-                this.props.navigation.navigate('DashboardScreen')
+                this.props.navigation.navigate('PantalladePanel')
             } else {
-                this.props.navigation.navigate('LoginScreen')
+                this.props.navigation.navigate('PantalladeIniciodeSesión')
             }
         })
     }
